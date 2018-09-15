c3a3e958bc5e784c1a4f233591cc4dd295606f46
baikadamova.alua <baikadamova.alua@gmail.com>
2018-09-15 07:59:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  
+  return console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  
+  return console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  var a=console.log(string)
+if (string.toUpperCase===a) then
+{
+return "YES INDEED!";
+}
+if (string.toLowerCase===a) then
+{
+return "I can't hear you!";
+} 
+ 
+if ((string.toUpperCase!=a) and (string.toLowerCase!=a)) then
+{
+return "I love you, too."
+}
+}
