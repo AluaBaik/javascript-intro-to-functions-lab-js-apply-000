45ce414e25ace7d231f1c6b6c03f46fc04171f55
baikadamova.alua <baikadamova.alua@gmail.com>
2018-09-15 08:29:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ function logWhisper(string) {
   return console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  
-if (string.toUpperCase===string){
+  var a=console.log(string)
+if (string.toUpperCase===a){
   return console.log("YES, INDEED!")
 }
-
+if (string.toLowerCase===a){
+  return console.log("I can't hear you!")
+}
 
 
 }
